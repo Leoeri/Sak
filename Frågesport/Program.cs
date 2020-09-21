16598afa2943022b8b2d17e860b308848014848f
Leo Eriksson <leo.eriksson5@learnet.se>
2020-09-21 09:13:27 +0200

    Initial commit

---
+++ Frågesport/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Frågesport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = hello();
+            Console.WriteLine(h);
+            Console.ReadLine();
+        }
+
+        static int hello()
+        {
+            Console.WriteLine("Skriv nagot");
+            string s = Console.ReadLine();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(s);
+            return 4;
+        }
+    }
+}
